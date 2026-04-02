9335c71ae7fb88038af9ab0ecbd7cfbe09df264f
Durga Kiran <85632819+D-Kiran@users.noreply.github.com>
2026-04-02 10:02:34 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+
+        int n = s.Length;
+        int result = 0;
+        int mostf = -1;
+
+       var dict = new Dictionary<char,int>();
+       int left =0;
+       for(int right=0; right<n; right++){
+          if(!dict.ContainsKey(s[right])){
+            dict[s[right]] =1;
+          }
+          else {
+            dict[s[right]]++;
+          }
+          mostf = Math.Max(mostf, dict[s[right]]);  //FindMostOccuringCharacterFrequency(dict);
+         Console.WriteLine($"mostf {mostf}");
+         var windowlength = right-left+1;
+         if(windowlength - mostf <= k){
+            result = Math.Max(result,windowlength);
+         }
+         else{
+            
+            dict[s[left]]--;
+            left++;
+            
+         }
+
+       }
+       return result;
+    }
+
+    private int FindMostOccuringCharacterFrequency(Dictionary<char,int> dict){
+        int max = -1;
+        foreach(var kvp in dict){
+            max = Math.Max(kvp.Value, max);
+        }
+        return max;
+    }
+}
