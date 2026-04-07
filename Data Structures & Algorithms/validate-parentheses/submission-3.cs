14c234c0d226992310440ae65cf7c109c5c8fe9d
Durga Kiran <85632819+D-Kiran@users.noreply.github.com>
2026-04-07 09:30:37 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        var stack = new Stack<char>();
+
+        for(int i=0; i<s.Length; i++){
+            var ch = s[i];
+
+            if(ch == '[' || ch == '(' || ch == '{'){
+                stack.Push(ch);
+            }
+            if(stack.Count == 0) return false;
+            else if (ch == ']' ){
+               // if(stack.Count == 0) return false;
+                var popped = stack.Pop();
+                if(popped != '[') return false;               
+            }
+             else if (ch == '}'){
+               // if(stack.Count == 0) return false;
+                var popped = stack.Pop();
+                if(popped != '{') return false; 
+
+            }
+             else if (ch == ')'){
+               // if(stack.Count == 0) return false;
+                var popped = stack.Pop();
+                if(popped != '(') return false; 
+            }
+        }
+
+        return stack.Count ==0;
+    }
+}
