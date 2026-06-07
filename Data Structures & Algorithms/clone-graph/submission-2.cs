f9333c4e80d172aaac72ca82295eed85d5656286
Durga Kiran <85632819+D-Kiran@users.noreply.github.com>
2026-06-07 23:05:42 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.cs
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+public class Solution {
+
+    private Dictionary<Node,Node> map = new();
+
+    public Node CloneGraph(Node node) {
+        if(node == null) return null;
+        map[node] = new Node(node.val);
+
+        var queue = new Queue<Node>();
+
+        queue.Enqueue(node);
+
+        while(queue.Count > 0){
+            var current = queue.Dequeue();
+
+            foreach(var nei in current.neighbors){
+                if(!map.ContainsKey(nei)){
+                    map[nei] = new Node(nei.val);
+                    queue.Enqueue(nei);
+                }
+                map[current].neighbors.Add(map[nei]);
+            }
+
+        }
+
+        return map[node];    
+    }
+}
