13f51e47faf8cd20fc92a530d17a510563898189
Durga Kiran <85632819+D-Kiran@users.noreply.github.com>
2026-06-23 10:58:20 +0530

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    private int[][] directions = [[-1,0],[1,0],[0,-1],[0,1]];
+    private int ROWS,COLS;
+
+    public int MaxAreaOfIsland(int[][] grid) {
+        ROWS = grid.Length;
+        COLS = grid[0].Length;
+        int area = 0;
+
+        for(int r=0; r<ROWS; r++){
+            for(int c=0 ;c<COLS; c++){
+                if(grid[r][c] == 1){
+                    area = Math.Max(area, Dfs(r,c,grid));
+                }
+            }
+        }
+
+        return area;
+    }
+
+    private int Dfs(int r, int c, int[][] grid){
+        if(r < 0 || c <0 || r == ROWS || c== COLS || grid[r][c] == 0) return 0;
+
+        grid[r][c] = 0;
+
+        int area = 1;
+
+        foreach(var dir in directions){
+            area += Dfs(r+dir[0], c+dir[1],grid);
+        }
+
+        return area;
+      
+    }
+}
